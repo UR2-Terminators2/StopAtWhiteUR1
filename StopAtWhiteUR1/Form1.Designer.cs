30f4cd548d3bc0156e0489f963fe414731b40c2e
Luisx09 <lrodrigue@ltu.edu>
2015-09-02 17:23:48 -0400

    Initial commit

---
+++ StopAtWhiteUR1/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace StopAtWhiteUR1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(157, 62);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(724, 447);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(427, 557);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 47);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(714, 536);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(166, 26);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            this.numericUpDown1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.numericUpDown1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(751, 570);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Threshold";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(658, 590);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(267, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "0 = 0 threshold; 100 = 255 threshold.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(119, 554);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1039, 637);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ StopAtWhiteUR1/Form1.cs
@@ -0,0 +1,200 @@
+﻿/***********************************************
+ * 
+ * Creater: Joey Yudasz 
+ * Date: 4/21/2015
+ * Class: UR1
+ * TERM PROJECT ( LINE FOLLOW )
+ * 
+ ***********************************************/
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO.Ports;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace StopAtWhiteUR1
+{
+    public partial class Form1 : Form
+    {
+        enum State { enabled, disabled };
+        State s = State.disabled;
+        SerialPort serPort;
+        string ComPort = "COM4"; // Port on laptop
+        delegate void displayStringDelegate(Label l, String s);
+        Capture _capture = null;
+        int threshold = 125;
+        int totNumPixels = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            this.KeyDown += Form1_KeyDown;
+            numericUpDown1.Value = (threshold * 100) / 255;
+            totNumPixels = imageBox1.Width * imageBox1.Height;   
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            serPort = new SerialPort(ComPort);
+            serPort.BaudRate = 9600;
+            serPort.DataBits = 8;
+            serPort.Parity = Parity.None;
+            serPort.StopBits = StopBits.One;
+            serPort.Open();
+            serPort.DataReceived += new SerialDataReceivedEventHandler(serPort_DataReceived);
+            _capture = new Capture();
+            _capture.ImageGrabbed += Display_Captured;
+            _capture.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (s == State.enabled) //Starting the Run 
+            {
+                s = State.disabled;
+                button1.Text = "Start";
+                serPort.Write("X");
+            }
+            else // disabled
+            {
+                s = State.enabled;
+                button1.Text = "Stop";
+                serPort.Write("S");
+            }
+        }
+
+        void Display_Captured(object sender, EventArgs e)
+        {
+            Image<Gray, Byte> gi = _capture.RetrieveGrayFrame().Resize(imageBox1.Width, imageBox1.Height, Emgu.CV.CvEnum.INTER.CV_INTER_LINEAR);
+            Image<Gray, Byte> gray_frame = _capture.RetrieveGrayFrame().Resize(imageBox1.Width, imageBox1.Height, Emgu.CV.CvEnum.INTER.CV_INTER_LINEAR);
+            Image<Gray, Byte> bw_frame = gray_frame.ThresholdBinary(new Gray(threshold), new Gray(255));
+            imageBox1.Image = bw_frame;  // CheckWhitePix(bw_frame);
+            gi = gi.ThresholdBinary(new Gray(threshold), new Gray(255));
+            imageBox1.Image = gi;
+
+            // Setting all start Values to Zero
+            int num_white_left = 0; 
+            int num_white_middle = 0;
+            int num_white_right = 0;
+            int percent_white = 0;
+            int segmentSize = imageBox1.Width / 3;
+            int segmentArea = segmentSize * imageBox1.Height;
+
+            for (int h = 0; h < imageBox1.Size.Height / 3; h++)
+            {
+                for (int w = 0; w < imageBox1.Size.Width; w++)
+                {
+                    if (gi.Data[h, w, 0] == 255) //-- Grayscale image
+                    {
+                        if (w < segmentSize) //Shows Amount of Pixels in left 3rd
+                        {
+                            num_white_left++; //Counts the number
+                        }
+                        else if (w <= (segmentSize * 2)) //shows amount of pixels in middle 3rd
+                        {
+                            num_white_middle++; //Counts the number
+                        }
+                        else // if (w >= (segmentSize *2)) shows amount of pixels in right 3rd
+                        {
+                            num_white_right++; // counts the number 
+                        }
+                    }
+                }
+            }
+            percent_white = (num_white_left + num_white_middle + num_white_right) * 100 / totNumPixels;
+
+            dispStr(label1, "L: " + num_white_left.ToString() + "  " + "M: " + num_white_middle.ToString()
+                + "  " + "R: " + num_white_right.ToString() + "  " + "%White: " + percent_white.ToString());
+            // Sgowing the comands on the output screen 
+
+            if (s == State.enabled)
+            {
+
+                // Strings for all the outputs of the command label 
+                string TurnRight = "Turn Right";
+                string GoStraight = "Go Straight Forward";
+                string TurnLeft = "Turn Left";
+                string Stop = "Stop";
+              
+
+                // if statment that compairs the num of white right to middle and left 
+                if ((num_white_right > num_white_middle))
+                {
+                    dispStr(label2, "" + TurnLeft.ToString()); // outputs turn left
+                    serPort.Write("L"); // sends L to atmel
+                }
+                // if statment that compairs the num of white left to middle and right 
+                if ((num_white_left > num_white_right))
+                {
+                    dispStr(label2, "" + TurnRight.ToString()); // outputs turn right 
+                    serPort.Write("R"); //sends R to atmel 
+                }
+                // if statment that compairs the num of white on the left and the right to the middle values 
+                if ((num_white_middle < num_white_left) && (num_white_middle < num_white_right))
+                {
+                    dispStr(label2, "" + GoStraight.ToString()); // output to go straight
+                    serPort.Write("S");//sends S to atmel 
+                }  
+                //Stop at end
+                if (num_white_middle > (segmentArea * 0.25))
+                {
+                    dispStr(label2, "" + Stop.ToString());
+                    serPort.Write("X");
+                    s = State.disabled;
+                }
+            }
+        }
+        void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                threshold += 10;
+                if (threshold >= 255)
+                    threshold = 255;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                threshold -= 10;
+                if (threshold <= 0)
+                    threshold = 0;
+            }
+        }
+        void serPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            threshold = (Convert.ToInt32(numericUpDown1.Value) * 255) / 100;
+        }
+
+        private void numericUpDown1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                threshold += 10;
+                if (threshold >= 255)
+                    threshold = 255;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                threshold -= 10;
+                if (threshold <= 0)
+                    threshold = 0;
+            }
+        }
+        public void dispStr(Label l, string s)
+        {
+            if (InvokeRequired)
+            {
+                displayStringDelegate dispStrDel = dispStr;
+                this.BeginInvoke(dispStrDel, l, s);
+            }
+            else
+                l.Text = s;
+        }
+    }
+}
